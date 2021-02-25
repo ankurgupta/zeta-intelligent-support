5421f5695a263ce5b2fdb9c98adfd32c5c92694d
Ankur Gupta <agupta@zetaglobal.com>
2021-02-25 13:02:09 +0530

    ruby version

--- app/jobs/intelligent_support_deligator_job.rb
+++ app/jobs/intelligent_support_deligator_job.rb
@@ -2,6 +2,10 @@ class IntelligentSupportDeligatorJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+    puts args
+    sleep 20
+    puts "***********"
+
     # Do something later
   end
 end
